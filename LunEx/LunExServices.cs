84b01f33549aecb8c5f341cdd73431d2dd10ce7c
Rob Myers <Rob.Myers@agileInstitute.com>
2023-02-28 13:02:03 -0800

    Revert "Scary rename of project file to approach what I have for (don't laugh) VB.Net..."
    This reverts commit 0fae11ec8bdb541d9cfa51c9a630fd8483b9a8dc.

---
+++ LunEx/LunExServices.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ITC {
+    public interface SecurityExchangeTransmissionInterface {
+        int CurrentPrice(string symbol);
+    }
+}
+
+namespace LunEx {
+    /// <summary>
+    /// UNTOUCHABLE!  You cannot change this file.
+    /// @see ITC.SecurityExchangeTransmissionInterface
+    /// </summary>
+    public class LunExServices : ITC.SecurityExchangeTransmissionInterface {
+        private static Random invisibleHand = new Random();
+
+        public virtual int CurrentPrice(string symbol) {
+            pauseToEmulateSendReceive();
+            if (invisibleHand.Next(100) > 80)
+                throw new LunExServiceUnavailableException();
+            return 103 + invisibleHand.Next(20);
+        }
+
+        private void pauseToEmulateSendReceive() {
+            System.Threading.Thread.Sleep(5000);
+        }
+    }
+    public class LunExServiceUnavailableException : Exception {
+        public override string Message {
+            get {
+                return "Sorry, sunspot activity today...please try again later";
+            }
+        }
+    }
+}
